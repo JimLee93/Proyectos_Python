b1ec9d8abca2c5f8b05de4bcbd94580f25cb611d
Jim <jmlparles@gmail.com>
2025-10-03 23:26:02 +0200

    prueba3

--- U1_Introduccion/FP_Hoja_01/ejercicio15.py
+++ U1_Introduccion/FP_Hoja_01/ejercicio15.py
@@ -1,8 +1,5 @@
 """
 Ejercicio 15
-Escribir un algoritmo para calcular la nota final de un estudiante, considerando que por cada respuesta
-correcta 5 puntos, por una incorrecta -1 y por respuestas en blanco 0. Imprime el resultado obtenido
-por el estudiante.
 """
 def calcular_nota(respuestas_correctas, respuestas_incorrectas, respuestas_en_blanco):
     """
